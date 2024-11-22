56dad0f3d876feb0e87658de955c46480cd5ef60
oiDreas <andreasanthony92@gmail.com>
2024-11-22 17:24:22 -0300

    First Init

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const fs = require('fs');
 const path = "./Bíblia Sagrada-utf8.txt"
 fs.readFile(path, 'utf8', (err, data) => {
@@ -18,4 +19,26 @@ fs.readFile(path, 'utf8', (err, data) => {
 
 const getVerse = (text, book, chapter, verse) => {
     let livro = text.find(`»${book}`);
+=======
+const fs = require('fs');
+const path = "./Bíblia Sagrada-utf8.txt"
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo:', err);
+        return;
+    }
+    getVerse(data, "GÊNESIS")
+    var book = data.find(`»${book}`);
+    // O conteúdo do arquivo está em data
+    console.log(data);
+
+    // Agora você pode processar o texto da Bíblia, por exemplo:
+    const versiculos = data.split('\n');
+    console.log('Número de versículos:', versiculos.length);
+});
+
+
+const getVerse = (text, book, chapter, verse) => {
+    let livro = text.find(`»${book}`);
+>>>>>>> 60109d3 (First Init)
 }
