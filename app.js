bfd4d4ad7e51cb2baa5a11ea7f1a5de7085bc4fc
Andreas Anthony <andreasanthony92@gmail.com>
2024-11-22 17:39:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = "./Bíblia Sagrada-utf8.txt"
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo:', err);
+        return;
+    }
+    getVerse(data, "GÊNESIS")
+    var book = data.find(`»${book}`);
+    // O conteúdo do arquivo está em data
+    console.log(data);
+
+    // Agora você pode processar o texto da Bíblia, por exemplo:
+    const versiculos = data.split('\n');
+    console.log('Número de versículos:', versiculos.length);
+});
+
+
+const getVerse = (text, book, chapter, verse) => {
+    let livro = text.find(`»${book}`);
+}
